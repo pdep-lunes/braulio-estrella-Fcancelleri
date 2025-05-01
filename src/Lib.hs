e666aff04a8ec52227fda191ea740e5834760a55
Federico Cancelleri <fcancelleri@frba.utn.edu.ar>
2025-05-01 17:49:59 -0300

    Agrego la funcion BolaEspinosa sin errores

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,7 +8,7 @@ data Personaje = UnPersonaje {
     superPoder :: String,
     superPoderActivo :: Bool,
     vida :: Int
-    } deriving Show
+    } deriving (Show,Eq)
 
 
 espina :: Personaje
@@ -18,7 +18,6 @@ espina = UnPersonaje{
         superPoder = "Granada de Espinas",
         superPoderActivo = True,
         vida = 4800
-
     }
 
 pamela :: Personaje
@@ -30,6 +29,9 @@ pamela = UnPersonaje{
     vida = 9600
 }
 
-bolaEspinosa :: Personaje -> Int
-bolaEspinosa personaje = (vida personaje) - 1000
+modificarVida :: Int -> Personaje -> Personaje --recibe la nueva vida del personaje y el personaje para poder modificar. Devuelve el personaje con su vida modificada--
+modificarVida nuevaVida (UnPersonaje nombre poderBasico superPoder superPoderActivo _) = UnPersonaje nombre poderBasico superPoder superPoderActivo nuevaVida
+
+bolaEspinosa :: Personaje -> Personaje
+bolaEspinosa personaje = modificarVida (vida personaje - 1000) personaje
 
