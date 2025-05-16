023ff6ed98bcca769ccc1760b6091ef4ffe6ccf5
Federico Cancelleri <fcancelleri@frba.utn.edu.ar>
2025-05-16 11:53:00 -0300

    Cambio todo el codigo. Empiezo de 0. Realizo todos los poderes y superpoderes, y realizo nuevas funciones auxiliares. Hago los reportes.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,76 +2,102 @@ module Lib () where
 
 import Text.Show.Functions ()
 
+type Poder = Personaje -> Personaje
+
+
 data Personaje = UnPersonaje {
     nombre :: String,
-    poderBasico :: String, 
-    superPoder :: String,
+    poder :: [Poder],
+    superPoder :: Poder,
     superPoderActivo :: Bool,
-    vida :: Float
-    } deriving (Show,Eq)
+    vida :: Int
+    } deriving Show
 
 
 espina :: Personaje
 espina = UnPersonaje{
-        nombre = "Espina",
-        poderBasico = "Bola de Espinas",
-        superPoder = "Granada de Espinas",
+        nombre = "Spike",
+        poder = [bolaEspinosa],
+        superPoder = granadaDeEspinas(5),
         superPoderActivo = True,
         vida = 4800
     }
 
 pamela :: Personaje
 pamela = UnPersonaje{
-    nombre = "Pamela",
-    poderBasico = "Lluevia de tuercas sanadoras",
-    superPoder = "Torreta Curativa",
+    nombre = "Pam",
+    poder = [lluviaTuercasSanadoras,lluviaTuercasDañinas],
+    superPoder = torretaCurativa,
     superPoderActivo = False,
     vida = 9600
 }
 
-modificarVida :: Float -> Personaje -> Personaje --recibe la nueva vida del personaje y el personaje para poder modificar. Devuelve el personaje con su vida modificada--
-modificarVida nuevaVida (UnPersonaje nombre poderBasico superPoder superPoderActivo _) = UnPersonaje nombre poderBasico superPoder superPoderActivo nuevaVida
+-- FUNCIONES AUXILIARES
+cambiarVida :: (Int -> Int) -> Personaje -> Personaje
+cambiarVida fn unPersonaje = unPersonaje {vida = ((fn.vida) unPersonaje)}
 
-bolaEspinosa :: Personaje -> Personaje
-bolaEspinosa personaje = modificarVida (vida personaje - 1000) personaje
+agregarANombre :: (String -> String) -> Personaje -> Personaje
+agregarANombre fn unPersonaje = unPersonaje {nombre = nombre ++ fn}
 
-{-lluviaDeTuercas: pueden ser sanadoras o dañinas. 
-Las primeras le suman 800 puntos de vida a su colega y 
+cambiarSuper :: (Bool -> Bool) -> Personaje -> Personaje
+cambiarSuper fn unPersonaje = unPersonaje {superPoderActivo = fn}
+
+--PODERES Y SUPERPODERES
+
+--bolaEspinosa: le quita 1000 puntos de vida a quien sea su contrincante (¡no debe quedar un número negativo!).
+
+bolaEspinosa :: Poder
+bolaEspinosa unPersonaje = (cambiarVida(subtract 1000)) unPersonaje
+
+{-
+pueden ser sanadoras o dañinas. Las primeras le suman 800 puntos de vida a su colega y 
 las segundas le disminuyen a la mitad la vida de quien sea su contrincante. 
 En cualquier otro caso, no le pasa nada al personaje.
 -}
 
-lluviaDeTuercas :: Personaje -> Bool -> Personaje
-lluviaDeTuercas personaje esColega
-    |esColega == True = modificarVida (vida personaje + 800) personaje
-    |esColega == False = modificarVida ((vida personaje) /2) personaje
+lluviaTuercasDañinas :: Poder
+lluviaTuercasDañinas unPersonaje  = (cambiarVida(subtract (div (vida unPersonaje) 2)) unPersonaje)
 
-{-lluviaDeTuercasDañinas :: Personaje -> Personaje
-lluviaDeTuercasDañinas personaje = modificarVida ((vida personaje) /2) personaje
-
-lluviaDeTuercasSanadoras :: Personaje -> Personaje
-lluviaDeTuercasSanadoras personaje = modificarVida (vida personaje + 800) personaje-}
+lluviaTuercasSanadoras :: Poder
+lluviaTuercasSanadoras unPersonaje = (cambiarVida(+800) unPersonaje)
 
 {-granadaDeEspinas: el daño va a depender del radio de explosión de la misma. 
 Si es mayor a 3, le agregara a su nombre “Espina estuvo aquí”. 
 Si además su contrincante tiene menos de 800 vida, desactiva su súper y lo deja con 0 de vida. 
 En otro caso, se usa una bola de espinas.
 -}
-modificarSuperYVida :: Float -> Bool -> Personaje -> Personaje
-modificarSuperYVida nuevaVida ulti (UnPersonaje nombre poderBasico superPoder _ _) = UnPersonaje nombre poderBasico superPoder ulti nuevaVida
 
-modificarNombre :: Personaje -> Personaje
-modificarNombre (UnPersonaje nombre poderBasico superPoder superPoderActivo vida) = UnPersonaje (nombre ++ ", Espina estuvo aqui") poderBasico superPoder superPoderActivo vida
+granadaDeEspinas :: (Int -> Int) -> Poder
+granadaDeEspinas RadioExplosion unPersonaje 
+    |RadioExplosion > 3 && vida UnPersonaje < 800 = (agregarANombre("Espina estuvo aqui").cambiarSuper(False).cambiarVida(subtract (vida unPersonaje))) unPersonaje
+    |otherwise = bolaEspinosa unPersonaje
+{-
+torretaCurativa: le activa el súper a su aliado y lo deja con el doble de su salud inicial.
+-}
 
-granadaDeEspinas :: Float -> Personaje -> Personaje
-granadaDeEspinas radioExplosion personaje
-    | radioExplosion > 3 = modificarNombre personaje
-    | vida personaje < 800 = (modificarSuperYVida 0 False personaje)
-    | otherwise = bolaEspinosa personaje
+torretaCurativa :: Poder
+torretaCurativa unPersonaje = (cambiarSuper(True).cambiarVida(*2)) unPersonaje
 
-{-torretaCurativa: le activa el súper a su aliado y lo deja con el doble de su salud inicial.-}
+--REPORTES
 
-torretaCurativa :: Personaje -> Bool -> Personaje
-torretaCurativa personaje esAliado
-    |esAliado == True = modificarSuperYVida (vida personaje * 2) True personaje
+{-atacar con el poder especial: si el personaje tiene el súper poder activo, 
+entonces va a atacar a su contrincante con el súper y con el básico. Si no, no hará nada.-}
 
+atacarConPoderEspecial :: Personaje -> Poder
+atacarConPoderEspecial unContrincante unPersonaje
+    |superPoderActivo unPersonaje = ((superPoder unPersonaje).(poder unPersonaje) unContrincante)
+    |otherwise = unContrincante
+
+{-
+saber quiénes están en las últimas: 
+es decir, el nombre de aquellos brawlers que tienen menos de 800 puntos de vida
+-}
+
+personajesQueEstanEnLasUltimas :: Personaje -> Bool
+personajesQueEstanEnLasUltimas unPersonaje = (vida unPersonaje) < 800
+
+estanEnLasUltimas :: [Personaje] -> [String]
+estanEnLasUltimas personajes = nombresDePersonajesQueEstanLasUltimas personajes
+
+nombresDePersonajesQueEstanLasUltimas :: [Personajes] -> [String]
+nombresDePersonajesQueEstanLasUltimas personajes = (map (nombre) . filter (personajesQueEstanEnLasUltimas)) Personajes
